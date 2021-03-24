18c331a60328006f2ba81d18a546948680356ea0
EdinaDinka <cuturaedina@gmail.com>
2021-03-24 09:26:56 +0100

    Added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+const firstName = (firstName) => {
+    var uppercase = firstName.toUpperCase();
+    return uppercase;
+
+}
+const lastName = (lastName) => {
+    var lovercase = lastName.toLowerCase();
+    return lovercase;
+
+}
+exports.firstName = firstName;
+exports.lastName = lastName;
+console.log(firstName("Edina"));
+console.log(lastName("Cutura"));
