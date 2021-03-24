4844cd2eb8882f06b41d2a378f3659a3e8fabb13
EdinaDinka <cuturaedina@gmail.com>
2021-03-24 09:30:54 +0100

    Added ex07

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,12 @@
+const firstName = (firstName) => {
+    var uppercase = firstName.toUpperCase();
+    return uppercase;
+
+}
+const lastName = (lastName) => {
+    var lovercase = lastName.toLowerCase();
+    return lovercase;
+
+}
+exports.firstName = firstName;
+exports.lastName = lastName;
